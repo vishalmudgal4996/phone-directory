199d8e8bf0f3d51e8dad77e99da0ed631b5ba161
vishalmudgal4996 <vishalmudgal4996@gmail.com>
2020-08-14 22:39:12 +0530

    Created ‘Header’ Component

--- src/App.js
+++ src/App.js
@@ -2,24 +2,29 @@ import React, { Component } from "react";
 import { Fragment } from "react";
 import { Button } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./Header";
 
 class App extends Component {
   render() {
     return (
       <Fragment>
-        {/* <div className="header">Phone Directory</div>
+        <Header />
         <Button variant="primary">Add</Button>
-        <div>
-          <span>Name</span>
-          <br />
-          <span>Phone</span>
-        </div> */}
+        <br />
         <label htmlFor="name">Name: </label>
         <input
           id="name"
           type="text"
           placeholder="Your Name"
           defaultValue="DesmonD"
+        ></input>
+        <br />
+        <label htmlFor="phone">Phone: </label>
+        <input
+          id="phone"
+          type="number"
+          placeholder="Your Phone Number"
+          defaultValue="8888888888"
         ></input>
       </Fragment>
     );
