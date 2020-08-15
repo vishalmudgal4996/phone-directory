9bb8552f9805a59c642764ca72ccfac66496aaa6
vishalmudgal4996 <vishalmudgal4996@gmail.com>
2020-08-15 13:25:33 +0530

    Styling: Styled 'App' (Home Page)

--- src/App.js
+++ src/App.js
@@ -3,29 +3,37 @@ import { Fragment } from "react";
 import { Button } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./Header";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <Fragment>
         <Header />
-        <Button variant="primary">Add</Button>
-        <br />
-        <label htmlFor="name">Name: </label>
-        <input
-          id="name"
-          type="text"
-          placeholder="Your Name"
-          defaultValue="DesmonD"
-        ></input>
-        <br />
-        <label htmlFor="phone">Phone: </label>
-        <input
-          id="phone"
-          type="number"
-          placeholder="Your Phone Number"
-          defaultValue="8888888888"
-        ></input>
+        <div className="component-body-container">
+          <Button className="btn btn-success">Add</Button>
+          <div className="grid-container heading-container">
+            <label className="grid-item name-heading" htmlFor="name">
+              Name
+            </label>
+            <input
+              id="name"
+              type="text"
+              placeholder="Your Name"
+              defaultValue="DesmonD"
+            ></input>
+            <br />
+            <label className="grid-item phone-heading" htmlFor="phone">
+              Phone
+            </label>
+            <input
+              id="phone"
+              type="number"
+              placeholder="Your Phone Number"
+              defaultValue="8888888888"
+            ></input>
+          </div>
+        </div>
       </Fragment>
     );
   }
